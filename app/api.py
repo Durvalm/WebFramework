4c3715d840b51271156e818e405c3ab989d31ef3
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2024-01-28 18:14:54 -0500

    url kwargs

--- app/api.py
+++ app/api.py
@@ -1,3 +1,4 @@
+import json
 import uvicorn
 from starlette.responses import Response
 from starlette.requests import Request
@@ -25,23 +26,31 @@ class API:
             if kwargs is not None:
                 return pattern, kwargs
         return None, {}
+    
+    def _dispatch(self, request):
+        pattern, kwargs = self._find_route(request.url.path)
+        route = self._routes.get(pattern)
+        if route is None:
+            raise HTTPException
+        
+        response_obj = route._handler(**kwargs)
+        json_data = json.dumps({'data': response_obj})
+        response = Response(json_data, media_type='application/json')
+        return response
+
 
     def run(self, host='0.0.0.0', port=5200):
         print(f'Serving App on port {host}:{port}')
         uvicorn.run(self, host=host, port=port)
 
+
     async def __call__(self, scope, receive, send):
         assert scope['type'] == 'http'
         request = Request(scope, receive)
 
-        pattern, kwargs = self._find_route(request.url.path)
-        route = self._routes.get(pattern)
-        if route is None:
-            raise HTTPException
-
-        response_obj = route._handler()
-        response = Response(response_obj, media_type='text/plain')
+        response = self._dispatch(request)
         await response(scope, receive, send)
 
-        return response_obj
+
+
 
--- example.py
+++ example.py
@@ -1,23 +1,24 @@
-import json
-from starlette.responses import JSONResponse
 from app.api import API
 
 api = API()
 
-@api.route("/")
-def get_users():
-    data = [
+data = [
         {'id': 1, 'name': 'Durval Maia', 'age': 19},
         {'id': 2, 'name': 'Davi Dantas', 'age': 9},
         {'id': 3, 'name': 'Rafael Nadal', 'age': 35}
     ]
 
-    json_data = json.dumps({'data': data})
-    return json_data
+@api.route("/users")
+def get_users():
+    return data
 
-@api.route("/oi")
-def oi():
-    return {'message': 'oi'}
+@api.route("/user/{user_id:d}")
+def get_user(user_id):
+    for user in data:
+        if user['id'] == user_id:
+            result = user
+
+    return result
 
 if __name__ == '__main__':
     api.run(host="127.0.0.1", port=5050)
